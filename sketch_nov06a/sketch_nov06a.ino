eaed569c29e2d4d2b7892b6f89ccde0652ee9ac6
Pravleen <47741189+Pravleen@users.noreply.github.com>
2020-11-16 18:27:24 +0530

    Add files via upload

---
+++ sketch_nov06a/sketch_nov06a.ino
@@ -0,0 +1,87 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>;
+#include <WiFiClient.h>;
+#include <ThingSpeak.h>;
+
+const char* ssid = "ABC"; //Your Network SSID
+const char* password = "12345678911"; //Your Network Password
+int val;
+int PulseSensorpin = A0; //Pulse Sensor Pin Connected at A0 Pin
+int buzzer=8;
+WiFiClient client;
+unsigned long start_time;
+unsigned long time1;
+unsigned long myChannelNumber = 1220585 ; //Your Channel Number (Without Brackets)
+const char * myWriteAPIKey = "Q6IVPLEKDSUWPQBF"; //Your Write API Key
+
+void setup()
+{
+ Serial.begin(9600);
+ start_time=millis();
+ delay(10);
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  ThingSpeak.begin(client);
+}
+
+void loop()
+{
+ int flag=1;
+  val = analogRead(PulseSensorpin); //Read Analog values and Store in val variable
+  Serial.println("Pulse Sensorvalue=  "); // Start Printing on Pulse sensor value on LCD
+  Serial.println(val); // Start Printing on Pulse sensor value on LCD
+  delay(10);
+  if(analogRead(PulseSensorpin)>=140 || analogRead(PulseSensorpin)<=55)
+  {
+  while(flag==1)
+  {
+    time1=millis();
+    if((time1-start_time)>=060000)
+    {
+    Serial.print("Critical condition");
+    digitalWrite(buzzer, HIGH);
+    while (get_http(String("Critical")) != 0);
+    }
+    if(analogRead(PulseSensorpin)<=140 || analogRead(PulseSensorpin)>=55)
+    {
+      flag=0;
+      break;
+  }
+}
+}
+  digitalWrite(buzzer,LOW);
+  ThingSpeak.writeField(myChannelNumber, 1,val, myWriteAPIKey); //Update in ThingSpeak
+  
+  
+}
+int get_http(String state)
+{
+   HTTPClient http;
+   int ret = 0;
+   Serial.print("[HTTP] begin...\n");
+   // configure ifttt server and url  should be HTTP only..not https!!!  (http://)   
+    //http.begin("https://maker.ifttt.com/trigger/Pulse sensor/with/key/fnaGeMWtqDtLb0fR7xZXWVu1ItGam_P13cdnqCPzo2g"); //HTTP
+   //If you have enabled value1 from iftt settings then uncomment below line to send value and comment out above line  
+   http.begin("http://maker.ifttt.com/trigger/Pulse/with/key/fnaGeMWtqDtLb0fR7xZXWVu1ItGam_P13cdnqCPzo2g="+state); //HTTP
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+    // httpCode will be negative on error
+    if(httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET code: %d\n", httpCode);
+
+      if(httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    } else {
+        ret = -1;
+        Serial.printf("[HTTP] GET failed, error: %s\n", http.errorToString(httpCode).c_str());
+        delay(500); // wait for half sec before retry again
+    }
+
+    http.end();
+    return ret;
+}  
